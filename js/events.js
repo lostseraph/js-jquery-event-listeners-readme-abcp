81acc0162f7c1a99e8d7a248343971b96a452a12
Roscoe Sabale <rsabale43@gmail.com>
2018-05-15 03:03:59 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -2,13 +2,13 @@
 function getIt() {
   $('p').on('click', function() {
     alert("Hey!");
-  })
+  });
 }
 
 function frameIt() {
   $('img').on('load', function() {
     $('img').addClass('tasty');
-  })
+  });
 }
 
 function pressIt() {
@@ -16,11 +16,13 @@ function pressIt() {
     if(key.which === 71) {
       alert('G was pressed');
     }
-  })
+  });
 }
 
 function submitIt() {
-  $
+  $('form').on('submit', function() {
+    alert('Your form is going to be submitted now.');
+  });
 }
 
 $(document).ready(function(){
