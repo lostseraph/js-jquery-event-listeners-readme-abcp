69995b2f8b16d845fa58a22cfbed15545c727f1d
Roscoe Sabale <rsabale43@gmail.com>
2018-05-15 02:51:34 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,4 +1,17 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert("Hey!");
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty');
+  })
+}
+
+
 
 $(document).ready(function(){
 
